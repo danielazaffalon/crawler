0aa920064031396d9775c56ea5bafefdf150c3dd
Daniela Zaffalon <daniela.zaffalon@onemindng.com>
2024-06-25 16:40:30 +0200

    Use cheerios to parse data

--- scraping.js
+++ scraping.js
@@ -1,3 +1,5 @@
+import { load } from 'cheerio';
+
 /** get html text from the web */
 async function getRawData(){
     const response = await fetch('https://news.ycombinator.com/');
@@ -5,10 +7,29 @@ async function getRawData(){
     return body;
 }
 
+/** parse the html text and extract titles */
+async function parseData(data){
+    const dataArray = [];
+    const $ = load(data);
+
+    $('tr.athing').each((_,item) => {
+        const itemNode = $(item);
+        const entry = {};
+        entry.number = Number(itemNode.find('span.rank').text().split('.')[0]);
+        entry.title = itemNode.find('span.titleline').find('a').text();
+        entry.points = Number(itemNode.next().find('span.score').text().split(' ')[0]);
+        entry.comments = Number(itemNode.next().find('span.subline').find('a:contains("comments")').text().split(/\s|&nbsp;/g)[0]);
+        dataArray.push(entry);
+    });
+    
+    return dataArray;
+}
+
 /** Execute Function */
 async function execute(){
     const data = await getRawData();
-    console.log(data);
+    const rta = await parseData(data)
+    console.log(rta);
 }
 
 execute();
