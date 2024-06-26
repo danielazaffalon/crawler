16c90cda47002f780dffb5913b2af2e62d901db9
Daniela Zaffalon <daniela.zaffalon@onemindng.com>
2024-06-26 18:22:03 +0200

    Service & comunication with firestore DB

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { Crawler } from "./scraping.js";
 import { LogEntry } from "./log.js";
+import { StorageService } from "./storageService.js";
 
 /** Execute Function */
 async function execute(){
 
-    const logEntry = new LogEntry();//"requestedFilter"
+    const logEntry = new LogEntry();//"requestedFilter" from terminal
+    const storageService = new StorageService();
     const crawler = new Crawler('https://news.ycombinator.com/');
+    
     const rawData = await crawler.getRawData();
     const entries = crawler.dataScraping(rawData);
     logEntry.fillAttribute(entries.length,"totalCrawledEntries");
@@ -13,6 +16,7 @@ async function execute(){
     logEntry.fillAttribute(filteredEntries.length,"totalFilteredEntries");
     logEntry.endLog();
 
+    await storageService.insertLogEntry(logEntry.exportLog());
     // console.log(filteredEntries);
     console.log(logEntry.exportLog());
 }
---
+++ storageService.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import config from './config.js';
+import {
+    getFirestore,
+    collection,
+    doc,
+    addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+  } from 'firebase/firestore';
+
+export class StorageService {
+
+    firebase = null;
+    db = null;
+
+    constructor(){
+        this.firebase = initializeApp(config.firebaseConfig);
+        this.db = getFirestore(this.firebase);
+    }
+
+    async insertLogEntry(logEntry){
+        await addDoc(collection(this.db,"crawlerLog"), logEntry);
+    }
+}
